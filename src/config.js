8d70447f17db0e96af5071d01be7f2892ae6f0b0
DEEPAK ACHARYA <56695253+deepakacharyab@users.noreply.github.com>
2023-05-22 12:18:46 -0500

    resolved conflicts

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     cloudWatchUrlBase: 'https://d29dkgeibpazuv.cloudfront.net/',
-    version: 'v0.0.2.1',
-    sourceIMGUrl: 'https://data.ghrc.earthdata.nasa.gov/ghrcw-public/'
+    sourceIMGUrl: 'https://data.ghrc.earthdata.nasa.gov/ghrcw-public/',
+    version: 'v0.0.2.1'
 }
 
 export default config
